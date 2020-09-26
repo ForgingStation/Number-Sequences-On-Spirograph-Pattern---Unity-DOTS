14ccb5f9ee09fd2706ac0e6fcea1d27706386471
Forging Station <67524557+ForgingStation@users.noreply.github.com>
2020-09-26 11:45:52 +0100

    Add files via upload

---
+++ Assets/Scripts/AtomicMovementSystem.cs
@@ -0,0 +1,85 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class AtomicMovementSystem : SystemBase
+{
+    private static bool Initialized = false;
+    private float changeEvery = 0;
+    private float ratioqn = 5;
+    private int largeRadius = 30;
+    private int smallRadius = 20;
+    private float ratioqd = 3;
+    private bool isNegetive;
+
+    protected override void OnUpdate()
+    {
+        float deltaTime = Time.DeltaTime;
+        if (!Initialized)
+        {
+            largeRadius = Pattern_Generator.instance.largeRadius;
+            smallRadius = Pattern_Generator.instance.smallRadius;
+            ratioqd = Pattern_Generator.instance.initialRatioQd;
+            ratioqn = Pattern_Generator.instance.initialRatioQn;
+            changeEvery = Pattern_Generator.instance.changeEvery;
+            Initialized = true;
+        }
+        changeEvery -= Time.DeltaTime;
+
+        if (changeEvery <= 0 && Initialized)
+        {
+            //ratioqn = ratioqn + 1; //Natural - 1
+            ratioqn = (ratioqn - 1) + (ratioqn - 2); // Fib-5
+            //ratioqn = (ratioqn - 2) + (ratioqn - 3); //Padovan-5
+            //ratioqn = (ratioqn - 1) + ratioqn; //Recamán's sequence-1
+            //ratioqn = (2*(ratioqn - 1)) + (ratioqn - 2); //Pell-3
+
+            ratioqd = (ratioqd - 1) +(ratioqd - 2);//Fib-4
+            //ratioqd = (ratioqd - 2) + (ratioqd - 3); //Padovan-2
+            //ratioqd = (ratioqd - 1) + ratioqd;//Recamán's sequence-2
+            //ratioqd = (2 * (ratioqd - 1)) + (ratioqd - 2);//Pell-2
+
+            float ratioQnForJob = ratioqn;
+            int largeRadiusForJob = largeRadius;
+            int smallRadiusForJob = smallRadius;
+            float ratioqdForJob = ratioqd;
+            if (!isNegetive)
+            {
+                ratioQnForJob = ratioQnForJob * -1;
+                isNegetive = true;
+            }
+            else
+            {
+                isNegetive = false;
+            }
+            Entities.WithBurst().ForEach((ref AtomicUnit au) =>
+            {
+                float xValue = (largeRadiusForJob * math.cos(au.entityId)) +
+                                (smallRadiusForJob * (math.cos(((ratioQnForJob) / ratioqdForJob) * au.entityId)));
+
+                float zValue = (largeRadiusForJob * math.sin(au.entityId)) +
+                                (smallRadiusForJob * (math.sin(((ratioQnForJob) / ratioqdForJob) * au.entityId)));
+                au.toLocation = new float3(xValue, 0, zValue);
+                au.reached = false;
+                au.ratioQnDebug = ratioQnForJob;
+             }).ScheduleParallel();
+            changeEvery = Pattern_Generator.instance.changeEvery;
+        }
+
+        Entities.WithBurst().ForEach((ref AtomicUnit au, ref Translation trans) =>
+        {
+            if (!au.reached)
+            {
+                au.waypointDirection = math.normalize(au.toLocation - trans.Value);
+                trans.Value += au.waypointDirection * au.Speed * deltaTime;
+
+                if (math.distance(trans.Value, au.toLocation) <= au.minDistanceReached)
+                {
+                    au.reached = true;
+                }
+            }
+        }).ScheduleParallel();
+    }
+
+}
---
+++ Assets/Scripts/Pattern_Generator.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Rendering;
+using Unity.Mathematics;
+using System.Collections.Generic;
+
+public class Pattern_Generator : MonoBehaviour
+{
+    public int maxUnitsToSpawn;
+    public int initialRatioQn;
+    public int initialRatioQd;
+    public int largeRadius;
+    public int smallRadius;
+    public float unit_MinDistanceReached;
+    public int unit_MinSpeed;
+    public int unit_MaxSpeed;
+    public Mesh mesh;
+    public Material material;
+    public float changeEvery;
+    public static Pattern_Generator instance { get; private set; }
+
+    private EntityManager entitymamager;
+    private EntityArchetype ea;
+    private Vector3 destinationToSet;
+    private Vector3 currentPosition;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        entitymamager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        ea = entitymamager.CreateArchetype(
+                    typeof(Translation),
+                    typeof(Rotation),
+                    typeof(LocalToWorld),
+                    typeof(RenderMesh),
+                    typeof(RenderBounds),
+                    typeof(AtomicUnit)
+                    );
+        currentPosition = transform.position;
+        for (int i=1;i<=maxUnitsToSpawn;i++)
+        {
+            Entity e = entitymamager.CreateEntity(ea);
+            entitymamager.AddComponentData(e, new Translation
+            {
+                Value = currentPosition
+            });
+            entitymamager.AddComponentData(e, new AtomicUnit
+            {
+                entityId = i,
+                minDistanceReached = unit_MinDistanceReached,
+                Speed = UnityEngine.Random.Range(unit_MinSpeed, unit_MaxSpeed),
+            });
+            entitymamager.AddSharedComponentData(e, new RenderMesh
+            {
+                mesh = mesh,
+                material = material,
+                castShadows = UnityEngine.Rendering.ShadowCastingMode.On
+            });
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(10, 10, 400, 40), "Fibonacci Series");
+        GUI.skin.box.fontSize = 25;
+    }
+}
+
+public struct AtomicUnit : IComponentData
+{
+    public int entityId;
+    public float3 toLocation;
+    public bool reached;
+    public float3 waypointDirection;
+    public float Speed;
+    public float minDistanceReached;
+    public float ratioQnDebug;
+}
+
+
+
+
+
+
